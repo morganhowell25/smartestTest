18b194d897590a27d5007584a69682c628971900
Tim D <tim.daigle13@yahoo.com>
2017-05-16 17:36:16 -0400

    updated manageuserscene

--- SmartestTest/src/smartesttest/ManageUserScene.java
+++ SmartestTest/src/smartesttest/ManageUserScene.java
@@ -21,38 +21,34 @@ import javafx.scene.layout.GridPane;
  *
  * @author csc190
  */
-public class ManageUserScene extends AdminDash
-{
-    public ManageUserScene()
-    {
-        
+public class ManageUserScene extends AdminDash {
+
+    public ManageUserScene() {
+
     }
-            
-    public Scene getScene()
-    {
+
+    public Scene getScene() {
         GridPane gp = drawAdminDash();
-        
+
         Label userID = new Label("ID");
         Label userRole = new Label("Role");
         Label userName = new Label("Username");
         Label delete = new Label("Change Password?");
-        gp.add(userID,1,0);
-        gp.add(userRole,2,0);
-        gp.add(userName,3,0);
-        gp.add(delete,4,0);
-        
-        
-        
+        gp.add(userID, 1, 0);
+        gp.add(userRole, 2, 0);
+        gp.add(userName, 3, 0);
+        gp.add(delete, 4, 0);
+
         ArrayList<ArrayList<String>> arrUsers = server.pullUserList();
-        
-        for (int i = 0; i < arrUsers.size(); i++){
-            for (int j = 0; j < arrUsers.get(i).size(); j++){
+
+        for (int i = 0; i < arrUsers.size(); i++) {
+            for (int j = 0; j < arrUsers.get(i).size(); j++) {
                 Label myLbl = new Label((arrUsers.get(i)).get(j));
-                gp.add(myLbl,1+i,1+j);
-                final int buttonInd = j+1;
+                gp.add(myLbl, 1 + i, 1 + j);
+                final int buttonInd = j + 1;
                 Button delBtn = new Button("Change Password");
-        
-                delBtn.setOnAction(new EventHandler<ActionEvent>() { 
+
+                delBtn.setOnAction(new EventHandler<ActionEvent>() {
                     @Override
                     public void handle(ActionEvent event) {
                         System.out.println("ChangePass Clicked!");
@@ -61,22 +57,38 @@ public class ManageUserScene extends AdminDash
                         dialog.setHeaderText(null);
                         dialog.setContentText("Please enter new password:");
                         Optional<String> result = dialog.showAndWait();
-                        if (result.isPresent()){
-                            server.resetPWD(result.get(),buttonInd);
-                        }
-                        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                        alert.setTitle("Password Changed Successfully");
-                        alert.setHeaderText(null);
-                        alert.setContentText("Press OK to continue");
+                        if (result.isPresent()) {
+                            boolean validPass = true;
+                            for (int i = 0; i < result.get().length(); i++) {
+                                if (!((result.get().charAt(i) >= 'a' && result.get().charAt(i) <= 'z') || (result.get().charAt(i) >= 'A' && result.get().charAt(i) <= 'Z') || (result.get().charAt(i) >= '0' && result.get().charAt(i) <= '9'))) {
+                                    validPass = false;
+                                    break;
+                                }
+                            }
+                            if (validPass == false) {
+                                Alert badAlert = new Alert(Alert.AlertType.ERROR);
+                                badAlert.setTitle("Failed to Add User");
+                                badAlert.setHeaderText("Invalid password");
+                                badAlert.setContentText("Please only use alphanumeric characters");
+                                badAlert.showAndWait();
+                            } else {
+                                server.resetPWD(result.get(), buttonInd);
 
-                        alert.showAndWait();
+                                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                                alert.setTitle("Password Changed Successfully");
+                                alert.setHeaderText(null);
+                                alert.setContentText("Press OK to continue");
+
+                                alert.showAndWait();
+                            }
+                        }
                     }
                 });
-                gp.add(delBtn,4,1+j);
+                gp.add(delBtn, 4, 1 + j);
             }
-            
+
         }
-        
+
         ScrollPane sp = new ScrollPane();
         sp.setContent(gp);
         Scene scene = new Scene(sp, 700, 500);
